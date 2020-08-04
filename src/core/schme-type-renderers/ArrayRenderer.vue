49bd5d2531d783838fa02b74a17c71b826ed07f0
Jokcy <jokcylou@hotmail.com>
2020-08-04 17:15:25 +0800

    deep clone array default

--- src/core/schme-type-renderers/ArrayRenderer.vue
+++ src/core/schme-type-renderers/ArrayRenderer.vue
@@ -56,6 +56,7 @@
 
 <script lang="ts">
 import { Component, Watch } from 'vue-property-decorator'
+import clone from 'lodash.clonedeep'
 // import { getDefaultValueOfSchema } from '../utils'
 import { RendererBaseClass } from '../mixins'
 
@@ -89,7 +90,7 @@ export default class JsfArrayRenderer extends RendererBaseClass {
     if (!this.value || !Array.isArray(this.value)) {
       // this.$emit('input', this.schema.default || [])
       const defaultValue = this.isMultiType ? [] : undefined
-      this.handleChange(this.schema.default || defaultValue)
+      this.handleChange(clone(this.schema.default) || defaultValue)
     } else {
       this.currentValue = this.value
     }
